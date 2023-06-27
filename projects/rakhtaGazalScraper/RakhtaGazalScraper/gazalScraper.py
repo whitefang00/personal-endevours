ec25b6099e8aef422e5f8f5917486ed4d4d7c4cf
whitefang00 <dna00@berkeley.edu>
2023-06-27 12:58:12 -0700

    first project file commit

---
+++ projects/rakhtaGazalScraper/RakhtaGazalScraper/gazalScraper.py
@@ -0,0 +1,13 @@
+
+from selenium import webdriver
+from selenium.common import TimeoutException
+
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.support import expected_conditions as EC, wait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+import pandas as pds
+from selenium.webdriver.support.wait import WebDriverWait
+import threading
